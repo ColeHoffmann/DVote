a2a81ff7bdfb69fc3425cc836b79a9e82e261a52
ColeHoffmann <46608689+ColeHoffmann@users.noreply.github.com>
2021-12-09 23:59:18 -0500

    Did work on bank contract

--- contracts/BankingContact.sol
+++ contracts/BankingContact.sol
@@ -1,6 +1,16 @@
 pragma solidity ^0.4.17;
 
 contract BankingContract { 
+    //addess of the owner
+    address public owner;
+
+
+    //counts of the candidates and voters
+    uint candidateCount, voterCount;
+
+    //election starte or ended. 
+    bool start, end; 
+
 
 
 
