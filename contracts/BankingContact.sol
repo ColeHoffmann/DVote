67c0c977103a361e62e550b0ce39b3fd7d852b0e
ColeHoffmann <46608689+ColeHoffmann@users.noreply.github.com>
2021-12-10 00:08:03 -0500

    Created the candidate struct within the smart contract.

--- contracts/BankingContact.sol
+++ contracts/BankingContact.sol
@@ -1,19 +1,35 @@
 pragma solidity ^0.4.17;
 
-contract BankingContract { 
+contract BankingContract {
     //addess of the owner
     address public owner;
 
-
     //counts of the candidates and voters
-    uint candidateCount, voterCount;
+    uint256 candidateCount = 0;
+    uint256 voterCount = 0;
 
-    //election starte or ended. 
-    bool start, end; 
+    //election starte or ended.
+    bool start = false;
+    bool end = false;
 
+    //return the address of the owner.
+    function getOwner() public view returns (address) {
+        return owner;
+    }
 
+    modifier adminOnly() {
+        require(msg.sender == owner);
+        _;
+    }
 
-
-
-
+    //Struct for a candidate;
+    struct Candidate {
+        //All the things a Candidate would have. Name, party affiliation, bio, voteCount, constituency, candidateID
+        string name;
+        string party;
+        string bio;
+        uint256 voteCount;
+        uint256 constituency;
+        uint256 candidateID;
+    }
 }
